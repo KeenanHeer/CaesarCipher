90e64568e14c58f7cebf07fb2c71273e99679582
KeenanHeer <keenan@Administrators-MacBook-Pro-2.local>
2021-02-07 18:32:46 +0000

    We made encrption work

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 ALPHABETS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def main():
-    text = input("What text do you want to covert? ").upper()
-    shift = int(input('how much would you like to shift the key by? '))
-    encryption(text,shift)
+    HowToPlay = ""
+    while HowToPlay != 'Z':
+        HowToPlay = input("press x to do encryption \nPress y to do decryption \nPress z to close the programme").upper()
+        if HowToPlay == 'X':
+            text = input("What text do you want to covert? ").upper()
+            shift = int(input('how much would you like to shift the key by? '))
+            print(encryption(text,shift))
+            
+        if HowToPlay == 'Y':
+            DecryptText = input('what text would you like to decrypt?').upper()
+    
+ 
+            
 
 
 def encryption(text,shift):
+    encrypted = []
     for textIndex in range(0,len(text)):
         indexOfCurrentElementBeignConverted = ALPHABETS.index(text[textIndex])
         shiftedIndex = indexOfCurrentElementBeignConverted + shift
         if shiftedIndex > 25:
             shiftedIndex = shiftedIndex-26
-        print(ALPHABETS[shiftedIndex])
-
-
+        encrypted.append(ALPHABETS[shiftedIndex])
+    return "".join(encrypted)
+    
 main()
